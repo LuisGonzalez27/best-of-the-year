2a7cce00355943e8633965f34aa5ecf856ad8150
LuisGonzalez27 <luis27coding@gmail.com>
2023-03-27 15:16:13 +0200

    Step 1

--- src/main/java/org/learning/bestoftheyear/controller/HomeController.java
+++ src/main/java/org/learning/bestoftheyear/controller/HomeController.java
@@ -1,13 +1,18 @@
-package org.learning.bestoftheyear.contoller;
+package org.learning.bestoftheyear.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    @GetMapping String home(){
+    @GetMapping String home(Model model){
+        model.addAttribute("name", "Luis");
         return "home";
     }
 }
